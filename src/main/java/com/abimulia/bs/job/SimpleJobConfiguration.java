12957338d0e1ab513124cec3b5473a76c800b9ac
abimulia <abimulia@gmail.com>
2024-12-01 11:54:59 +0700

    add run job with parameters

--- src/main/java/com/abimulia/bs/job/SimpleJobConfiguration.java
+++ src/main/java/com/abimulia/bs/job/SimpleJobConfiguration.java
@@ -4,6 +4,12 @@
  */
 package com.abimulia.bs.job;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -33,9 +39,18 @@ public class SimpleJobConfiguration {
 	public Step firstStep(JobRepository jobRepository, JdbcTransactionManager transactionManager) {
 		log.debug("firstStep() jobRepository: "+ jobRepository + " transactionManager: "+transactionManager);
 		return new StepBuilder("firstStep", jobRepository).tasklet((contribution, chunkContext) -> {
-			System.out.println("Simple Batch");
+			try {
+				String item = chunkContext.getStepContext().getJobParameters().get("item").toString();
+				String strdate = chunkContext.getStepContext().getJobParameters().get("run.date").toString();
+				LocalDate date = LocalDate.parse(strdate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+				LocalTime currentTime = LocalTime.now();
+				LocalDateTime dateTime = LocalDateTime.of(date, currentTime);
+				System.out.println(String.format("Simple Batch received %s on %s.",item,dateTime));
+			} catch (DateTimeParseException e) {
+				System.err.println("Failed to run job ["+e.getMessage()+"]");
+			}
 			return RepeatStatus.FINISHED; 
-		}, transactionManager).allowStartIfComplete(true).build();
+		}, transactionManager).build();
 	}
 
 	@Bean
